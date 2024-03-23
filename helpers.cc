86cb48783034b05df6d19d300f2ad6242f798b67
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-03-22 21:56:36 -0400

    CA-99 added documentation

--- helpers.cc
+++ helpers.cc
@@ -1,16 +1,16 @@
 #include "helpers.h"
 
-std::string trim(const std::string& str) {
+std::string trim(const std::string& _str) {
     std::string whitespace = " \t\r\n"; // Include carriage return and tab
-    size_t first = str.find_first_not_of(whitespace);
+    size_t first = _str.find_first_not_of(whitespace);
     if (first == std::string::npos) return "";
-    size_t last = str.find_last_not_of(whitespace);
-    return str.substr(first, (last - first + 1));
+    size_t last = _str.find_last_not_of(whitespace);
+    return _str.substr(first, (last - first + 1));
 }
 
-bool isComment(const std::string& line) {
+bool isComment(const std::string& _line) {
     // Check if the first non-whitespace character is '#'
-    for (char ch : line) {
+    for (char ch : _line) {
         if (std::isspace(ch)) continue; // Skip whitespace
         return ch == '#';
     }
--- helpers.h
+++ helpers.h
@@ -7,8 +7,27 @@
 #include <string>
 #include <vector>
 
-std::string trim(const std::string& str);
+/**
+ * @brief Trims a string of leading and trailing whitespace
+ * 
+ * @param[in] _str The string to be stripped
+ * 
+ * @return _str with whitespace removed
+ * 
+ * @note Whitespace is [" ", "\\t", "\\r", "\\n"]
+ */
+std::string trim(const std::string& _str);
 
-bool isComment(const std::string& line);
+/**
+ * @brief Checks if a line in a .csr file is a comment
+ * 
+ * @param[in] line The line to be checked
+ * 
+ * @return true if line is a comment
+ *         false otherwise
+ * 
+ * @note A line is a comment if the first non-whitespace character is "#"
+ */
+bool isComment(const std::string& _line);
 
 #endif
