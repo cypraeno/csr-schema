2e175bb826277650473244fd30a07926e9a5f8ad
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-03-23 18:04:04 -0400

    CA-99 minor reformatting

--- material_validator.h
+++ material_validator.h
@@ -15,10 +15,10 @@ const enum material_types {
 /**
  * @brief Checks if a file section starting with "Material" is a validly formatted .csr material
  * 
- * @param[in]   _file           the file being validated
- * @param[in]   _material_type  the type of material being validated
- * @param[in]   _textures       a vector of textures to be checked (lambertian surfaces)
- * @param[out]  _materials      a vector of materials to be updated if the material is valid
+ * @param[in]   _file           The file being validated
+ * @param[in]   _material_type  The type of material being validated
+ * @param[in]   _textures       A vector of textures to be checked (lambertian surfaces)
+ * @param[out]  _materials      A vector of materials to be updated if the material is valid
  * @return true if the material is correctly formatted
  *         false otherwise
  */
