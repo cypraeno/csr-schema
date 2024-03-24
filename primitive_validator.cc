55fca0e77d768d41f9906fb86593b64c5c984aca
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-03-24 01:18:01 -0400

    CA-99 completed primitive validation

---
+++ primitive_validator.cc
@@ -0,0 +1,53 @@
+#include "primitive_validator.h"
+
+struct vec3 { double x, y, z; };
+
+bool isQuad(std::ifstream& _file, std::vector<std::string>& _materials) {
+    
+    std::string line, keyword;
+    std::istringstream iss;
+    
+    // Check for valid position
+    if (!std::getline(_file, line)) return outputError("Error: Unexpected EOF");
+
+    iss.str(line);
+    if (!isXYZ(iss, "position", N_INF, P_INF)) return false;
+
+    // Check for valid u
+    if (!std::getline(_file, line)) return outputError("Error: Unexpected EOF");
+
+    iss.str(line);
+    if (!isXYZ(iss, "u", N_INF, P_INF)) return false;
+
+    // Assign vector for validation
+    iss.str(line);
+    vec3 u;
+    iss >> u.x >> u.y >> u.z;
+    
+    // Check for valid v
+    if (!std::getline(_file, line)) return outputError("Error: Unexpected EOF");
+
+    iss.str(line);
+    if (!isXYZ(iss, "v", N_INF, P_INF)) return false;
+
+    // Assign vector for validation
+    iss.str(line);
+    vec3 v;
+    iss >> v.x >> v.y >> v.z;
+
+    // Check valid uv orthogonality
+    if (u.x*v.x + u.y*v.y + u.z*v.z != 0.0) return outputError("Error: uv not orthogonal");
+
+    // Check for valid material
+    if (!std::getline(_file, line)) return outputError("Error: Unexpected EOF");
+
+    iss.str(line);
+    if (!(iss >> keyword) || keyword != "material") return outputError("Error: Expected material");
+
+    std::string material;
+    if (!(iss >> material)) return outputError("Error: No material id found");
+    if (material != "no") {
+        auto it = std::find(_materials.begin(), _materials.end(), material);
+        if (it == _materials.end()) return outputError("Error: Invalid material id");
+    }
+}
---
+++ primitive_validator.h
@@ -0,0 +1,35 @@
+#ifndef PRI_VALIDATOR
+#define PRI_VALIDATOR
+
+#include "helpers.h"
+#include "general_validators.h"
+
+/**
+ * @brief List of available primitive types (updated regularily)
+ */
+const enum primitive_types {
+    QUAD,
+    SPHERE,
+};
+
+/**
+ * @brief Checks if a file section starting with "Quad" is a validly formatted .csr quad
+ * 
+ * @param[in] _file         The file being validated
+ * @param[in] _materials    A vector of materials to be checked
+ * @return true if the quad is correctly formatted
+ *         false otherwise
+ */
+bool isQuad(std::ifstream& _file, std::vector<std::string>& _materials);
+
+/**
+ * @brief Checks if a file section starting with "Sphere" is a validly formatted .csr quad
+ * 
+ * @param[in] _file         The file being validated
+ * @param[in] _materials    A vector of materials to be checked
+ * @return true if the quad is correctly formatted
+ *         false otherwise
+ */
+bool isSphere(std::ifstream& _file, std::vector<std::string>& _materials);
+
+#endif
