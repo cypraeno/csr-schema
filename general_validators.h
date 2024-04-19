6f9e5f7709813d210853b6a8f3a3c2d1696cce1f
Samuel Bai <117124009+haenlonns@users.noreply.github.com>
2024-04-19 04:08:31 -0400

    CA-99 Add documentation

--- general_validators.h
+++ general_validators.h
@@ -4,12 +4,52 @@
 #include "helpers.h"
 #include <experimental/filesystem>
 
-bool isFilePath(std::stringstream& _ss);
+/**
+ * @brief Checks if a file line is a valid CSR file path of one of the specified file types
+ * 
+ * @param[in] _ss           The line being evaluated
+ * @param[in] _fileTypes    The list of valid file types
+ * 
+ * @return true if the string is a valid file path and type
+ *         false otherwise
+ * 
+ */
+bool isFilePath(std::stringstream& _ss, const std::vector<std::string>& _fileTypes);
 
+/**
+ * @brief Checks if a file line is a valid double between min and max
+ * 
+ * @param[in] _ss           The line being evaluated
+ * @param[in] _keyword      The keyword that the line must start with
+ * @param[in] _min, _max    The min and max of the double
+ * 
+ * @return true if line is a valid double between min and max
+ *         false otherwise
+ */
 bool isDouble(std::stringstream& _ss, const std::string& _keyword, const double _min, const double _max);
 
+/**
+ * @brief Checks if a file line is a set of three valid doubles between min and max
+ * 
+ * @param[in] _ss           The line being evaluated
+ * @param[in] _keyword      The keyword that the line must start with
+ * @param[in] _min, _max    The min and max of the doubles
+ * 
+ * @return true if line is a set of three valid doubles between min and max
+ *         false otherwise
+ */
 bool isXYZ(std::stringstream& _ss, const std::string& _keyword, const double _min, const double _max);
 
+/**
+ * @brief Checks if a file line is a valid ratio that has a value between min and max
+ * 
+ * @param _ss               The line being evaluated
+ * @param[in] _keyword      The keyword that the line must start with
+ * @param[in] _min, _max    The min and max of the ratio
+ * 
+ * @return true if line is a valid ratio between min and max (e.g. 16/9)
+ *         false otherwise
+ */
 bool isRatio(std::stringstream& _ss, const std::string& _keyword, const double _min, const double _max);
 
 // WIP need to consider abstraction when there is special cases (i.e. texture cannot have "no" as an id)
